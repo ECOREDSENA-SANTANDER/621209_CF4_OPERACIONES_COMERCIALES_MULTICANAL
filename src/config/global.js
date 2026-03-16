7d1081114fff3f1e4471e57f3a2ed4f34553d8dd
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-03-16 16:46:36 -0500

    Avance 1.0

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Manejo de inventarios en el <em>Retail</em>',
+    Description:
+      'La logística en <em>retail</em> radica en el conjunto de técnicas necesarias para poder cumplir con el objetivo final de acercar y vender un producto al cliente en su respectiva sala de ventas del formato comercial o <em>retail</em> especializado. Este proceso abarca desde la gestión eficiente del inventario y el almacenamiento, hasta la distribución y transporte del producto, asegurando su disponibilidad en el momento y lugar adecuados.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -12,6 +13,10 @@ export default {
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
       },
     ],
   },
@@ -31,27 +36,108 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Gestión logística de inventarios en el punto de venta',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Conceptos básicos',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Inventario: concepto, característica, clasificación',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Recursos: concepto, tipo, herramientas, clases',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Herramientas: concepto, clases, usos',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo: 'Código de barras: concepto, características, tipos ',
+            hash: 't_1_5',
+          },
+          {
+            numero: '1.6',
+            titulo:
+              '<em>Software</em> para la captura de código de barras: características, funcionalidad, tipos',
+            hash: 't_1_6',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Gestión de inventario',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Toma física: concepto, herramientas',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Toma digital: concepto, herramientas',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Técnicas de conteo: tipos, características, clases',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo:
+              'Mercancía: concepto, clases, normativa para manejo de mercancía',
+            hash: 't_2_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Gestión de productos',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Producto: concepto, naturaleza, atributos, clases',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Referencias: concepto, tipos',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Rotación: concepto, procedimiento',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Agotados: concepto, características',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.5',
+            titulo: '<em>Stock</em> de mercancía',
+            hash: 't_3_5',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Gestión de devolución',
         desarrolloContenidos: true,
       },
     ],
@@ -74,6 +160,11 @@ export default {
         titulo: 'Glosario',
       },
       {
+        nombreRuta: 'complementario',
+        icono: 'far fa-folder-open',
+        titulo: 'Material complementario',
+      },
+      {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
         nombreRuta: 'referencias',
@@ -81,7 +172,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_632223_DU.zip',
       },
       {
         icono: 'fas fa-download',
@@ -97,22 +188,107 @@ export default {
   },
   complementario: [
     {
-      tema: 'Tema 1',
-      referencia: 'Texto',
-      tipo: 'Sitio web',
-      link: 'Link',
+      tema: 'Gestión logística de inventarios en el punto de venta',
+      referencia: 'Puerta, L.F. (s.f.). Seminario manejo de carga.',
+      tipo: 'PDF',
+      link: 'downloads/seminario_de_carga.pdf',
+    },
+    {
+      tema: 'Gestión logística de inventarios en el punto de venta',
+      referencia:
+        'Compartamos banco. (2016). ¿Por qué es importante controlar el inventario? [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://youtu.be/AJaii3mpuJ4',
+    },
+    {
+      tema: 'Gestión de devolución',
+      referencia:
+        'Martínez, I. (2018). Aprende a hacer un formato para devolución de mercancía al almacén.',
+      tipo: 'Video',
+      link: 'https://youtu.be/RMqWGZY-PWM',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Almacén',
+      significado:
+        'lugar en el que se acumulan los productos de la empresa hasta que se venden.',
+    },
+    {
+      termino: 'Control',
+      significado:
+        'acción de revisión para comprobar que todo se ajusta a lo previsto y añadir las variaciones.',
+    },
+    {
+      termino: 'Demanda',
+      significado:
+        'reclamo del producto de la empresa por parte de su cliente.',
+    },
+    {
+      termino: 'Distribución',
+      significado:
+        'acción de poner a disposición del cliente el producto en el punto de venta.',
+    },
+    {
+      termino: 'Inventario',
+      significado: 'listado ordenado de la mercancía del almacén.',
+    },
+    {
+      termino: 'Materia prima',
+      significado:
+        'aquello que se incorpora al proceso de fabricación o producción y tras su transformación forma parte del producto.',
+    },
+    {
+      termino: 'Producto',
+      significado:
+        'lo que la empresa destina a la venta y forma parte de sus ingresos comerciales.',
+    },
+    {
+      termino: 'Rotura de <em>stock</em>',
+      significado:
+        'se produce cuando la demanda no puede ser satisfecha, ya que no se cuenta con mercancía en el almacén.',
+    },
+    {
+      termino: 'Salida de mercancía',
+      significado:
+        'registro por el que la mercancía sale del almacén de la empresa, principalmente, por las ventas.',
+    },
+    {
+      termino: 'Sistema de clasificación',
+      significado:
+        'sistema implementado en el almacén para ordenar sus existencias.',
+    },
+    {
+      termino: 'Sistema de valoración',
+      significado:
+        'sistema implementado en el almacén para poner precio a sus existencias.',
+    },
+    {
+      termino: '<em>Stock</em>',
+      significado: 'mercadería que se encuentra en el almacén de la empresa.',
+    },
+    {
+      termino: 'Transporte',
+      significado:
+        'conjunto de acciones que se desarrollan desde que la mercancía sale del almacén de la empresa hasta que llega al cliente.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Céspedes, A. (2010). Principios de mercadeo. Ecoe Ediciones. ',
+      link:
+        'https://archive.org/details/principiosdemerc0000cesp/page/466/mode/2up',
+    },
+    {
+      referencia: 'Kotler, P. (2017). Fundamentos del <em>marketing</em>.',
+      link:
+        'https://frrq.cvg.utn.edu.ar/pluginfile.php/14584/mod_resource/content/1/Fundamentos%20del%20Marketing-Kotler.pdf',
+    },
+    {
+      referencia:
+        'Xmarts. (2025, 13 febrero). Guía Completa de Control de Inventarios 2025: Qué es, Cómo Hacerlo y Ejemplos - Xmarts. Xmarts - Libera el potencial de tu empresa.',
+      link: 'https://xmarts.com/guia-completa-de-control-de-inventarios/',
     },
   ],
   creditos: [
@@ -120,12 +296,12 @@ export default {
       titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Claudia Johanna Gómez Pérez',
+          nombre: 'Milady Tatiana Villamil Castellanos',
           cargo: 'Responsable del ecosistema',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Edison Eduardo Mantilla Cuadros',
+          nombre: 'Claudia Johanna Gómez Pérez',
           cargo: 'Responsable de línea de producción',
           centro: 'Centro Agroturístico - Regional Santander',
         },
@@ -135,9 +311,37 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: '--',
-          cargo: 'Experto temático',
-          centro: '--',
+          nombre: 'Jaime Alberto Pérez Posada',
+          cargo: 'Instructor',
+          centro: 'Centro de comercio - Regional Antioquia',
+        },
+        {
+          nombre: 'Zvi Daniel Grosman',
+          cargo: 'Diseñador instruccional',
+          centro: 'Centro Agropecuario La Granja - Regional Tolima',
+        },
+        {
+          nombre: 'Ana Catalina Córdoba Sus',
+          cargo: 'Revisora metodológica y pedagógica',
+          centro:
+            'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano Reyes',
+          cargo: 'Asesor pedagógico',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'José Gabriel Ortiz Abella',
+          cargo: 'Corrector de estilo',
+          centro:
+            'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Sandra Paola Morales Páez',
+          cargo: 'Evaluadora instruccional',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
@@ -145,18 +349,18 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: '--',
-          cargo: 'Diseñador de contenidos',
+          nombre: 'Yazmin Rocio Figueroa Pacheco',
+          cargo: 'Diseñadora de contenidos digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: '--',
+          nombre: 'Pedro Alonso Bolivar Gonzalez',
           cargo: 'Desarrollador <em>full stack</em>',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
           nombre: 'Maria Alejandra Vera Briceño',
-          cargo: 'Animadora y productora audiovisual',
+          cargo: 'Animadora y productora multimedia',
           centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
@@ -165,13 +369,13 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Erika Daniela Manrique Rueda',
-          cargo: 'Validadora y vinculadora de recursos educativos digitales',
+          nombre: 'Yineth Ibette Gonzalez Quintero',
+          cargo: 'Validadora de recursos educativos digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: 'Laura Paola Gelvez Manosalva',
-          cargo: 'Evaluadora de contenidos inclusivos y accesibles',
+          nombre: 'Erika Fernanda Mejía Pinzón',
+          cargo: 'Evaluadora para contenidos inclusivos y accesibles',
           centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
@@ -183,4 +387,25 @@ export default {
     creativeCommons:
       'Licencia creative commons CC BY-NC-SA<br><a href="https://creativecommons.org/licenses/by-nc-sa/2.0/" target="_blank">ver licencia</a>',
   },
+  // creditosInicio: [
+  //   {
+  //     titulo: 'En alianza',
+  //     contenido: [
+  //       require('@/assets/template/logo-sena-naranja.svg'),
+  //       require('@/assets/template/mintic.jpg'),
+  //       require('@/assets/template/minsalud.jpg'),
+  //     ],
+  //   },
+  //   {
+  //     titulo: 'En compañía',
+  //     contenido: [
+  //       require('@/assets/template/presidencia.jpg'),
+  //       require('@/assets/template/ecopetrol.jpg'),
+  //     ],
+  //   },
+  //   {
+  //     titulo: 'Una iniciativa',
+  //     contenido: [require('@/assets/template/santander.jpg')],
+  //   },
+  // ],
 }
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -2,19 +2,21 @@
 .curso-main-container.introduccion
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  
+    .titulo-principal.color-secundario
       .titulo-principal__numero
         span
           i.fas.fa-info
       h1 Introducción
+      
+    p.mb-4 Bienvenidos al componente formativo Manejo de inventarios en el #[em Retail], en el cual se abordarán diversas temáticas con aspectos básicos y fundamentales que se deben tener en cuenta cuando se participa en las operaciones comerciales de una sala de ventas. A continuación, se invita a conocer un poco más de este componente formativo:
     
-    .row.justify-content-center.align-items-center           
-      .col-lg-12
-        figure
-          .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
-
+    .row.justify-content-center.align-items-center(data-aos="fade-left")
+      .col-lg-12.col-12.col-md-12
+        figure(data-aos="fade-right").py-2
+          .video.mb-1
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2mWqJONbPrs?si=nLXCLAdqQUtM3z0E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          figcaption Video. Manejo de inventarios en el #[em retail]: introducción
 </template>
 
 <script>
@@ -23,14 +25,6 @@ export default {
   data: () => ({
     // variables de vue
   }),
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-  updated() {
-    this.$aosRefresh()
-  },
 }
 </script>
 
